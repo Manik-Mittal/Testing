7615dd8d23d2283dabe24c3bddb9706af0c0873b
Abhishek-2400 <abhi2003shukla@gmail.com>
2024-03-09 16:35:40 +0530

    Image storage Engine for uploading  files in Upload folder

--- SkypeShop_backend/index.js
+++ SkypeShop_backend/index.js
@@ -9,17 +9,40 @@ const connectDB = require('./ConnectDb/connect')
 const notfound = require('./Middleware/notfound')
 require('dotenv').config();
 
+const port = process.env.PORT || 5000
+
 app.use(express.json())
 app.use(cors())
 
 
-const port = process.env.PORT || 4000
 
+//API CREATION
 app.get('/', (req, res) => {
     res.send('Express App is running')
 })
 
+//IMAGE STORAGE ENGINE
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './Upload/images')
+    },
+    filename: function (req, file, cb) {
+        cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    }
+})
 
+const upload = multer({ storage: storage })
+
+app.use('/images', express.static('./Upload/images'))
+app.post('/upload', upload.single('product'), (req, res) => {
+    res.json({
+        success: 1,
+        image_url: `http://localhost:${port}/images/${req.file.filename}`
+    })
+})
+
+
+//if no url satisfied
 app.use(notfound)
 
 const start = async () => {
