c8276513a24c3459794ab866e2d6b71b962802fa
Abhishek-2400 <abhi2003shukla@gmail.com>
2024-04-09 06:37:37 +0530

    Linked the Livestream page (client side) with backend

--- SkypeShop_frontend/src/Components/LiveStream/LiveStream.jsx
+++ SkypeShop_frontend/src/Components/LiveStream/LiveStream.jsx
@@ -1,12 +1,61 @@
 import React from 'react'
 import './LiveStream.css'
+import { useState } from 'react'
+import { useEffect } from 'react';
 const LiveStream = () => {
-    return (
-        <div>
-            <div className="live">
 
+    const [live, setlive] = useState([]);
+
+
+    const alllivestreams = async () => {
+        await fetch('http://localhost:5000/livestreams').then((Response) => {
+            if (!Response.ok) {
+                throw new Error("Unable to fetch livestreams")
+            }
+            return Response.json()
+        }).then((data) => {
+            console.log(data)
+            setlive(data.livestreams)
+        }).catch((err) => console.log(err))
+    }
+
+    const registeruse = () => {
+
+    }
+    useEffect(() => {
+        alllivestreams()
+    }, [])
+
+
+    return (
+
+        <div className="live">
+            <div className="live-head">
+                <h1>LiveStreams</h1>
             </div>
+
+            <div className="stream-container">
+                {live.map((val, index) => {
+                    return <div className="stream-card-content">
+                        <div className="stream-card-image">
+
+                            <img src={val.image} onClick={() => { registeruse() }}></img>
+
+                        </div>
+                        <div className="stream-card-text">
+                            <span>{val.name}</span>
+                            <br></br>
+                            <span>${val.old_price}  ${val.new_price}</span>
+                        </div>
+                    </div>
+
+
+                })}
+            </div>
+
+
         </div>
+
     )
 }
 
