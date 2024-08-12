c71daa7b2d2a174e1fe20c57a8b151ea6851825e
Abhishek-2400 <abhi2003shukla@gmail.com>
2024-08-13 00:06:16 +0530

    appointment booking integrated with backend and bug fixes in frontend

--- SkypeShop_frontend/src/Components/ProductDisplay/ProductDisplay.jsx
+++ SkypeShop_frontend/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -1,30 +1,54 @@
-import React from 'react'
-import '../ProductDisplay/ProductDisplay.css'
-import star_icon from '../Assets/star_icon.png'
-import star_dull_icon from '../Assets/star_dull_icon.png'
-import { useContext } from 'react'
-import { ShopContext } from '../../Context/ShopContext/ShopContext'
-import CalendarAppointment from '../../Pages/test'
+import React, { useEffect, useState } from 'react';
+import '../ProductDisplay/ProductDisplay.css';
+import star_icon from '../Assets/star_icon.png';
+import star_dull_icon from '../Assets/star_dull_icon.png';
+import { useContext } from 'react';
+import { ShopContext } from '../../Context/ShopContext/ShopContext';
+import CalendarAppointment from '../../Pages/test';
 
 const ProductDisplay = (props) => {
-
+    const [url, setUrl] = useState('');
 
     const { addTocart } = useContext(ShopContext);
     const helper = (id) => {
-        addTocart(id)
+        addTocart(id);
     }
-    // console.log(useContext(ShopContext, 1));
+
+    const fetchAdminImageBases = async () => {
+        try {
+            const response = await fetch('http://localhost:5000/getadminimagebases', {
+                method: 'POST',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ image: props.image })
+            });
+            if (!response.ok) {
+                throw new Error('Failed to get admin image bases');
+            }
+            const data = await response.json();
+            setUrl(data.response.url);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    useEffect(() => {
+        fetchAdminImageBases();
+    }, [props.image]); // Trigger fetch when `props.image` changes
+
     return (
         <div className='productdisplay'>
             <div className="pdleft">
                 <div className="minor">
-                    <img src={props.image}></img>
-                    <img src={props.image}></img>
-                    <img src={props.image}></img>
-                    <img src={props.image}></img>
+                    <img src={props.image} alt="Product Thumbnail" />
+                    <img src={props.image} alt="Product Thumbnail" />
+                    <img src={props.image} alt="Product Thumbnail" />
+                    <img src={props.image} alt="Product Thumbnail" />
                 </div>
                 <div className="major">
-                    <img src={props.image}></img>
+                    <img src={props.image} alt="Product" />
                 </div>
             </div>
             <div className="pdright">
@@ -32,16 +56,14 @@ const ProductDisplay = (props) => {
 
                 <div className="pdright_info">
                     <div className="stars">
-                        <img src={star_icon}></img>
-                        <img src={star_icon}></img>
-                        <img src={star_icon}></img>
-                        <img src={star_icon}></img>
-                        <img src={star_dull_icon}></img>
+                        <img src={star_icon} alt="Star Icon" />
+                        <img src={star_icon} alt="Star Icon" />
+                        <img src={star_icon} alt="Star Icon" />
+                        <img src={star_icon} alt="Star Icon" />
+                        <img src={star_dull_icon} alt="Dull Star Icon" />
                     </div>
                     <p>(122)</p>
                 </div>
-
-
 
                 <div className="product_display_price">
                     <p className='oldp'>${props.old_price}</p>
@@ -56,12 +78,13 @@ const ProductDisplay = (props) => {
                     Nam faucibus ipsum ut ante accumsan, non faucibus dolor fermentum.
                     Ut nec magna id mi pellentesque volutpat. Duis quis enim suscipit, vestibulum ligula vel, efficitur metus.
 
-                    <br></br>
-                    <br></br>
+                    <br />
+                    <br />
 
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                     Nullam in odio ultricies, fringilla nisi sed, pharetra metus.
-                    Maecenas ultrices velit id lectus scelerisque posuere.          Vestibulum volutpat enim ac justo lacinia, id euismod nunc molestie.
+                    Maecenas ultrices velit id lectus scelerisque posuere.
+                    Vestibulum volutpat enim ac justo lacinia, id euismod nunc molestie.
                     Nam faucibus ipsum ut ante accumsan, non faucibus dolor fermentum.
                 </div>
                 <div className="size_descirption">
@@ -74,18 +97,15 @@ const ProductDisplay = (props) => {
                         <div className="x">M</div>
                         <div className="x">L</div>
                     </div> */}
-                    <button onClick={() => { helper(props.id) }}>Add To Cart</button>
-                    <CalendarAppointment />
+                    <button onClick={() => helper(props.id)}>Add To Cart</button>
+                    <CalendarAppointment urlfrombackend={url} />
 
-                    <p><b>Category:</b>  Women,Tshirt,Crop Top</p>
-                    <p><b>Tags:</b>   Modern ,Latest</p>
-
-
-
+                    <p><b>Category:</b> Women, Tshirt, Crop Top</p>
+                    <p><b>Tags:</b> Modern, Latest</p>
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
-export default ProductDisplay
+export default ProductDisplay;
